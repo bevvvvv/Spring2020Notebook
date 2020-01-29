441b97365983def4c1952fa38919b49602e74630
bevvvvv <computerwhiz1337@gmail.com>
2020-01-29 07:27:42 -0500

    448 hw3 init

---
+++ CMPSC448/HW3/hw3.py
@@ -0,0 +1,13 @@
+"""
+CMPSC448 Machine Learning and AI Homework 3
+Randomization
+Author: Joseph Sepich (jps6444)
+"""
+def question1(num_models):
+    pass
+
+def question2(num_models):
+    pass
+    
+def question3(results):
+    pass
