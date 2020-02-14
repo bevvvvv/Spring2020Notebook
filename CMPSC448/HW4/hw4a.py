a737a3a1372109c8362e11da4ed7981664685580
bevvvvv <computerwhiz1337@gmail.com>
2020-02-14 14:49:39 -0500

    hw4a download

---
+++ CMPSC448/HW4/hw4a.py
@@ -0,0 +1,23 @@
+def question_matvec(mat, vec):
+   pass
+   
+def question_matmat(mat1, mat2):
+  pass
+  
+def question3(A,W,x,b):
+  pass
+  
+def question4(A,C,W,x,b,d):
+   pass
+   
+def question5(A,x,sigmoid):
+   pass
+   
+def question6(A,x,b,sigmoid):
+   pass
+   
+def question7(A,W,x,b,sigmoid):
+   pass
+   
+def question8(A,W,x,b,sigmoid,huber):
+   pass 
