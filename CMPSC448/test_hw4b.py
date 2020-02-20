8154b791c61ef9f3e7e98d579ed0e97783404375
bevvvvv <computerwhiz1337@gmail.com>
2020-02-19 20:39:57 -0500

    448 hw4b test file updated

--- CMPSC448/test_hw4b.py
+++ CMPSC448/test_hw4b.py
@@ -9,6 +9,28 @@ from matplotlib import pyplot as plt
 import pandas as pd
 import HW4.hw4b as hw
 
+class TestHWQuestions(unittest.TestCase):
+    def test_question1_loss(self):
+        pass
+
+    def test_question2_grad(self):
+        pass
+        
+    def test_question3_update(self):
+        pass
+        
+    def test_question4_n_updates(self):
+        pass
+        
+    def test_question5_nepochs(self):
+        pass
+        
+    def test_question6_sgd(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
+
 # this code is just a demonstration of functionality
 # make sure your code has no free - standing statements
 
@@ -16,7 +38,7 @@ import HW4.hw4b as hw
 # the oldfaithful.csv dataset is in the data directory in
 # canvas files
 
-def read_data () :
+def read_data():
     df = pd.read_csv("oldfaithful.csv", sep ="\s+", header ='infer', index_col=0)
     df.insert (0 , 'bias', 1) # add bias as first column
     df.insert (1 , 'sq', df.EruptionLength.values**2)
@@ -24,11 +46,14 @@ def read_data () :
     y = df.WaitingTime.values
     return X,y
 
+'''
+How to read data
 X, y = read_data()
+'''
 
 # how to shuffle data .
 # Make sure you have a data matrix X and target y
-def makeshuffler(seed=3) :
+def makeshuffler(seed=3):
     prng = np.random.RandomState(seed)
     def shuffle(X, y):
         indices = np.arange(y.size)
@@ -36,8 +61,11 @@ def makeshuffler(seed=3) :
         return X[indices], y[indices]
     return shuffle
 
+'''
+How to shuffle
 shuffle = makeshuffler()
 X, y = shuffle(X, y) # shuffled dataset
+'''
 
 def sigmoid(x):
     result = 1.0/(1 + np.exp(-np.array(x)))
@@ -48,6 +76,9 @@ def huber(x):
     result = np.where(np.abs(myval) <= 1, 0.5*myval**2 , np.abs(myval)-0.5)
     return result.sum()
 
+'''
+Example plot of huber function
 z = np.arange(-5, 5, 0.1)
 plt.plot(z, [huber(a) for a in z ])
 plt.show() # program will wait until you close the graph
+'''
