6b376f0e7de070f74b0565a0f94ac6baa52404ff
Joseph Sepich <computerwhiz1337@gmail.com>
2020-03-17 17:01:07 -0400

    442 some progress thru 3

--- CMPSC442/HW4/jps6444.py
+++ CMPSC442/HW4/jps6444.py
@@ -10,6 +10,9 @@ student_name = "Joseph Sepich"
 
 # Include your imports here, if any are used.
 import email
+import collections
+import math
+import os
 
 ############################################################
 # Section 1: Spam Filter
@@ -36,15 +39,62 @@ def load_tokens(email_path):
     return tokens
 
 def log_probs(email_paths, smoothing):
-    pass
+    """[summary]
+    
+    Arguments:
+        email_paths {list[string]} -- list of paths to email files
+        smoothing {float} -- smoothing factor alpha = 1 / m
+    
+    Returns:
+        [type] -- [description]
+    """
+    # load all tokens
+    tokens = []
+    for path in email_paths:
+        [tokens.append(token) for token in load_tokens(path)]
+    # create dictionary
+    word_count = collections.defaultdict(int)
+    total_count = len(tokens)
+    for token in tokens:
+        word_count[token] += 1
+    unique_count = len(word_count.keys())
+    probs = {}
+    for key in word_count.keys():
+        probs[key] = math.log((word_count[key] + smoothing) / (total_count + smoothing * (unique_count + 1)))
+    probs['<UNK>'] = math.log((smoothing) / (total_count + smoothing * (unique_count + 1)))
+    return probs
 
 class SpamFilter(object):
 
     def __init__(self, spam_dir, ham_dir, smoothing):
-        pass
+        spam_files = os.listdir(spam_dir)
+        ham_files = os.listdir(ham_dir)
+        spam_files = [spam_dir + f for f in spam_files]
+        ham_files = [ham_dir + f for f in ham_files]
+        self.cond_spam = log_probs(spam_files, smoothing)
+        self.cond_ham = log_probs(ham_files, smoothing)
+        self.p_spam = math.log(len(spam_files) / (len(spam_files) + len(ham_files)))
+        self.p_ham = math.log(len(ham_files) / (len(spam_files) + len(ham_files)))
     
     def is_spam(self, email_path):
-        pass
+        email_tokens = load_tokens(email_path)
+        # compute spam value
+        spam_val = self.p_spam
+        for token in email_tokens:
+            if token not in self.cond_spam.keys():
+                spam_val += self.cond_spam['<UNK>']
+            else:
+                spam_val += self.cond_spam[token]
+        # compute ham value
+        ham_val = self.p_ham
+        for token in email_tokens:
+            if token not in self.cond_ham.keys():
+                ham_val += self.cond_ham['<UNK>']
+            else:
+                ham_val += self.cond_ham[token]
+        if spam_val > ham_val:
+            return True
+        return False
 
     def most_indicative_spam(self, n):
         pass
--- CMPSC442/test_hw4.py
+++ CMPSC442/test_hw4.py
@@ -17,5 +17,23 @@ class TestHWQuestions(unittest.TestCase):
         self.assertEqual(hw.load_tokens(self.spam_dir+'spam1')[1:5],['You', 'are', 'receiving', 'this'])
         self.assertEqual(hw.load_tokens(self.spam_dir+'spam2')[:4],['<html>', '<body>', '<center>', '<h3>'])
 
+    def test_log_probs(self):
+        paths = [self.ham_dir+"ham%d" % i for i in range(1, 11)]
+        p = hw.log_probs(paths, 1e-5)
+        #self.assertEqual(p['the'], -3.6080194731874062)
+        #self.assertEqual(p['line'], -4.272995709320345)
+        paths = [self.spam_dir+"spam%d" % i for i in range(1, 11)]
+        p = hw.log_probs(paths, 1e-5)
+        self.assertEqual(p['Credit'], -5.837004641921745)
+        self.assertEqual(p['<UNK>'], -20.34566288044584)
+    
+    def test_is_spam(self):
+        sf = hw.SpamFilter(self.spam_dir, self.ham_dir, 1e-5)
+        self.assertTrue(sf.is_spam(self.spam_dir + 'spam1'))
+        self.assertTrue(sf.is_spam(self.spam_dir + 'spam2'))
+        sf = hw.SpamFilter(self.spam_dir, self.ham_dir, 1e-5)
+        self.assertFalse(sf.is_spam(self.ham_dir + 'ham1'))
+        self.assertFalse(sf.is_spam(self.ham_dir + 'ham2'))
+
 if __name__ == '__main__':
     unittest.main()
