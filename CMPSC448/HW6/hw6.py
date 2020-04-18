72e859ed247472d62e8d1a4504c58839725e79e2
Joseph Sepich <computerwhiz1337@gmail.com>
2020-04-17 21:14:32 -0400

    448 hw6 final

--- CMPSC448/HW6/hw6.py
+++ CMPSC448/HW6/hw6.py
@@ -27,11 +27,44 @@ X_train, X_valid, y_train, y_valid = train_test_split(train_df, train_cats, test
 
 # train model
 model = XGBClassifier(objective='multi:softprob',
-                        learning_rate = 0.01, # eta
+                        learning_rate = 0.02, # eta
                         max_depth = 6, # max tree depth
                         subsample = 0.8, #ratio of training data to use
-                        colsample_bytree = 0.5, # use less than all 93 features
+                        colsample_bytree = 0.3, # use less than all 93 features
+                        gamma = 5, # tree metric -> larger is stricter
                         n_estimators = 300) # num of trees
+
+""" hyper param tuning
+# Create parameter grid
+parameters = {"learning_rate": [0.1, 0.01],
+               "gamma" : [0.01, 0.1, 0.3, 0.5, 1, 1.5, 2],
+               "max_depth": [2, 4, 7, 10],
+               "colsample_bytree": [0.3, 0.6, 0.8, 1.0],
+               "subsample": [0.2, 0.4, 0.5, 0.6, 0.7],
+               "reg_alpha": [0, 0.5, 1],
+               "reg_lambda": [1, 1.5, 2, 3, 4.5],
+               "min_child_weight": [1, 3, 5, 7],
+               "n_estimators": [100, 250, 500]}
+
+# Create RandomizedSearchCV Object
+xgb_rscv = RandomizedSearchCV(model, param_distributions = parameters, scoring = "neg_log_loss", cv = 7, verbose = 3)
+
+# Fit the model
+model_xgboost = xgb_rscv.fit(X_train, y_train)
+
+# Model best estimators
+print("Learning Rate: ", model_xgboost.best_estimator_.get_params()["learning_rate"])
+print("Gamma: ", model_xgboost.best_estimator_.get_params()["gamma"])
+print("Max Depth: ", model_xgboost.best_estimator_.get_params()["max_depth"])
+print("Subsample: ", model_xgboost.best_estimator_.get_params()["subsample"])
+print("Max Features at Split: ", model_xgboost.best_estimator_.get_params()["colsample_bytree"])
+print("Alpha: ", model_xgboost.best_estimator_.get_params()["reg_alpha"])
+print("Lamda: ", model_xgboost.best_estimator_.get_params()["reg_lambda"])
+print("Minimum Sum of the Instance Weight Hessian to Make a Child: ",
+      model_xgboost.best_estimator_.get_params()["min_child_weight"])
+print("Number of Trees: ", model_xgboost.best_estimator_.get_params()["n_estimators"])
+"""
+
 model.fit(X_train, y_train,
             early_stopping_rounds = 5, # help prevent overfit
             eval_set = [(X_train, y_train), (X_valid, y_valid)],
