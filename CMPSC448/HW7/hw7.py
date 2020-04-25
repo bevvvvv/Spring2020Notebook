8eb901e2bee2248511d9561dbc1ddcc1dc4061d3
Joseph Sepich <computerwhiz1337@gmail.com>
2020-04-24 20:06:15 -0400

    448 write tests for final

--- CMPSC448/HW7/hw7.py
+++ CMPSC448/HW7/hw7.py
@@ -11,16 +11,17 @@ from fractions import Fraction as frac
 
 def question1Part1():
     pathverdict = [
-        (('a', 'b', 'd',), True) # HT blockage
-        (('a', 'e', 'd'), True) # HH blockage
+        (('a', 'b', 'd',), True), # HT blockage
+        (('a', 'e', 'd'), False), # none
+        (('a', 'e', 'f', 'g', 'c', 'd'), False) # none
     ]
-    result = True
+    result = False
     return result, pathverdict
 
 def question1Part2():
     pathverdict = [
-        (('a', 'b', 'd', 'c', 'g'), False) # none
-        (('a', 'b', 'e', 'f', 'g'), True) # HT blockage
+        (('a', 'b', 'd', 'c', 'g'), False), # none
+        (('a', 'b', 'e', 'f', 'g'), True), # HT blockage
         (('a', 'e', 'f', 'g'), True) # HT blockage
     ]
     result = False
@@ -28,8 +29,8 @@ def question1Part2():
 
 def question1Part3():
     pathverdict = [
-        (('a', 'b', 'd', 'c', 'g'), False) # none
-        (('a', 'b', 'e', 'f', 'g'), True) # HT blockage
+        (('a', 'b', 'd', 'c', 'g'), False), # none
+        (('a', 'b', 'e', 'f', 'g'), True), # HT blockage
         (('a', 'e', 'f', 'g'), True) # HT blockage
     ]
     result = False
@@ -37,8 +38,8 @@ def question1Part3():
 
 def question1Part4():
     pathverdict = [
-        (('a', 'b', 'd', 'c', 'g'), True) # HT blockage
-        (('a', 'b', 'e', 'f', 'g'), True) # HT blockage
+        (('a', 'b', 'd', 'c', 'g'), True), # HT blockage
+        (('a', 'b', 'e', 'f', 'g'), True), # HT blockage
         (('a', 'e', 'f', 'g'), False) # none
     ]
     result = False
@@ -46,7 +47,7 @@ def question1Part4():
 
 def question2Part1():
     pathverdict = [
-        (('a', 'b', 'c', 'd'), True) # HH blockage
+        (('a', 'b', 'c', 'd'), True), # HH blockage
         (('a', 'b', 'e', 'f', 'g', 'd'), True) # HT blockage
     ]
     result = True
@@ -54,7 +55,7 @@ def question2Part1():
 
 def question2Part2():
     pathverdict = [
-        (('a', 'b', 'c', 'd'), False) # none
+        (('a', 'b', 'c', 'd'), False), # none
         (('a', 'b', 'e', 'f', 'g', 'd'), True) # TT blockage
     ]
     result = False
@@ -62,24 +63,24 @@ def question2Part2():
 
 def question2Part3():
     pathverdict = [
-        (('b', 'c', 'd', 'g'), True) # TT blockage
+        (('b', 'c', 'd', 'g'), True), # TT blockage
         (('b', 'e', 'f', 'g'), True) # HT blockage
     ]
     result = True
     return result, pathverdict
 
 def question3Part1(a,b,c,d, bn):
-    p_a = bn.a(a)
-    p_b = bn.b(b)
-    p_c = bn.c(c, a, b)
-    p_d = bn.d(d, c)
+    p_a = bn.a(value=a)
+    p_b = bn.b(value=b)
+    p_c = bn.c(value=c, a=a, b=b)
+    p_d = bn.d(value=d, c=c)
 
     return p_a * p_b * p_c * p_d
 
 def question3Part2(a,b,c, bn):
-    p_a = bn.a(a)
-    p_b = bn.b(b)
-    p_c = bn.c(c, a, b)
+    p_a = bn.a(value=a)
+    p_b = bn.b(value=b)
+    p_c = bn.c(value=c, a=a, b=b)
 
     return p_a * p_b * p_c
 
@@ -96,23 +97,23 @@ def question3Part4(a,b,c, bn):
 
     return numerator / denonm
 
-def question3Part4(c, d, bn):
-    numerator = bn.d(d, c) * sum([question3Part2(i, j, c, bn) for i in range(2) for j in range(2)])
+def question3Part5(c, d, bn):
+    numerator = bn.d(value=d, c=c) * sum([question3Part2(i, j, c, bn) for i in range(2) for j in range(2)])
     denom = question3Part3(d, bn)
 
     return numerator / denom
 
 def question4Part1(b,c,d, bn):
-    p_d = bn.d(d, b, c)
+    p_d = bn.d(value=d, b=b, c=c)
 
-    return sum([bn.a(a) * bn.b(b, a) * bn.c(c, a) * p_d for a in range(2)])
+    return sum([bn.a(value=a) * bn.b(value=b, a=a) * bn.c(value=c, a=a) * p_d for a in range(2)])
 
 def question4Part2(c, d, bn):
-    return sum([bn.d(d, b, c) * bn.b(b, a) for b in range(2) for a in range(2)])
+    return sum([bn.d(value=d, b=b, c=c) * bn.b(value=b, a=a) for b in range(2) for a in range(2)])
 
 def question4Part3(d, e, bn):
-    p_ed = bn.e(e, d)
-    numerator = p_ed * sum([bn.d(b, c) * bn.b(b, a) * bn.c(c, a) * bn.a(a) for a in range(2) for b in range(2) for c in range(2)])
-    denom = sum([bn.e(e, d) * bn.d(b, c) * bn.b(b, a) * bn.c(c, a) * bn.a(a) for a in range(2) for b in range(2) for c in range(2)])
+    p_ed = bn.e(value=e, d=d)
+    numerator = p_ed * sum([bn.d(value=d, b=b, c=c) * bn.b(value=b, a=a) * bn.c(value=c, a=a) * bn.a(value=a) for a in range(2) for b in range(2) for c in range(2)])
+    denom = sum([bn.e(value=e, d=d) * bn.d(value=d, b=b, c=c) * bn.b(value=b, a=a) * bn.c(value=c, a=a) * bn.a(value=a) for a in range(2) for b in range(2) for c in range(2)])
 
     return numerator / denom
--- CMPSC448/test_hw7.py
+++ CMPSC448/test_hw7.py
@@ -6,7 +6,7 @@ Author: Joseph Sepich (jps6444)
 import unittest
 import numpy as np
 from fractions import Fraction as frac
-import HW7.hw7 as hw7
+import HW7.hw7 as hw
 
 ##############################################################################
 ################# REQUIRED DATA STRUCTURE ####################################
@@ -15,17 +15,17 @@ import HW7.hw7 as hw7
 class BayesNet3: # Problem 3 hw bayes net
     def __init__ (self, seed, k =10) :
         prng = np.random.RandomState(seed)
-        prob_a = frac(prng.randint(1, 2**k), 2**k) # P ( a =1)
-        prob_b = frac(prng.randint(1, 2**k), 2**k) # P ( b =1)
-        prob_c = {(0, 0): frac(prng.randint(1, 2**k), 2**k), # P ( c =1 | a =0, b =0)
+        self.prob_a = frac(prng.randint(1, 2**k), 2**k) # P ( a =1)
+        self.prob_b = frac(prng.randint(1, 2**k), 2**k) # P ( b =1)
+        self.prob_c = {(0, 0): frac(prng.randint(1, 2**k), 2**k), # P ( c =1 | a =0, b =0)
                 (0, 1): frac(prng.randint(1, 2**k ), 2**k), # P ( c =1 | a =0, b=1)
                 (1, 0): frac(prng.randint(1, 2**k ), 2**k), # P ( c =1 | a =0, b=1)
                 (1, 1): frac(prng.randint(1, 2**k ), 2**k) # P ( c =1 | a =0, b=1)
                 }
-        prob_d = {(0,): frac(prng.randint(1, 2**k), 2**k), # P ( d =1 | c =0)
+        self.prob_d = {(0,): frac(prng.randint(1, 2**k), 2**k), # P ( d =1 | c =0)
                 (1,): frac(prng.randint(1, 2**k ), 2**k), # P ( d =1 | c =1)
                 }
-        prob_e = {(0,): frac(prng.randint(1, 2**k), 2**k), # P ( e =1 | c =0)
+        self.prob_e = {(0,): frac(prng.randint(1, 2**k), 2**k), # P ( e =1 | c =0)
                 (1,): frac(prng.randint(1, 2**k ), 2**k), # P ( e =1 | c =1)
                 }
 
@@ -38,6 +38,8 @@ class BayesNet3: # Problem 3 hw bayes net
     def b(self, value): # returns P(b=value)
         if value == 1:
             return self.prob_b
+        else:
+            return 1 - self.prob_b
 
     def c(self, value, a, b): # returns P(c=value | a,b)
         tmp = self.prob_c[(a,b)]
@@ -66,8 +68,25 @@ class BayesNet3: # Problem 3 hw bayes net
 ##############################################################################
 
 class TestHWQuestions(unittest.TestCase):
-    def test_case(self):
+    def setUp(self):
+        self.bn3 = BayesNet3(3)
+        return super().setUp()
+
+    def test_question3Part1(self):
+        #self.assertEqual(hw.question3Part1(0, 0, 0, 0, self.bn3), 0)
         pass
+
+    def test_question3Part2(self):
+        pass
+
+    def test_question3Part3(self):
+        pass
+
+    def test_question3Part4(self):
+        pass
+
+    def test_question3Part5(self):
+        self.assertEqual(hw.question3Part5(0, 0, self.bn3), 0)
 
 
 if __name__ == '__main__':
