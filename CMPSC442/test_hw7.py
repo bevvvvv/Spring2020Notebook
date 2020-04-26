66c3cbbd981e990394e58039ad2609745a21cb50
Joseph Sepich <computerwhiz1337@gmail.com>
2020-04-25 22:15:58 -0400

    442 hw7 downloaded

---
+++ CMPSC442/test_hw7.py
@@ -0,0 +1,17 @@
+"""
+CMPSC 442 Sudoku Testing
+Author: Joseph Sepich (jps6444)
+Due April 27
+"""
+# import
+import unittest
+import HW1.jps6444 as hw
+
+# create test class
+class TestHWQuestions(unittest.TestCase):
+    def test(self):
+        pass
+
+# main function
+if __name__ == '__main__':
+    unittest.main()
